acc718694c48ff95a85730f537148169334c7975
Juan Banda <jbanda@deepml.cs.gsu.edu>
2020-02-05 02:36:06 -0500

    First push

--- data_acquisition/search_generic.py
+++ data_acquisition/search_generic.py
@@ -1,3 +1,21 @@
+# ███████╗ ██████╗  ██████╗██╗ █████╗ ██╗         ███╗   ███╗███████╗██████╗ ██╗ █████╗     ███╗   ███╗██╗███╗   ██╗██╗███╗   ██╗ ██████╗     ████████╗ ██████╗  ██████╗ ██╗     ██╗  ██╗██╗████████╗
+# ██╔════╝██╔═══██╗██╔════╝██║██╔══██╗██║         ████╗ ████║██╔════╝██╔══██╗██║██╔══██╗    ████╗ ████║██║████╗  ██║██║████╗  ██║██╔════╝     ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██║ ██╔╝██║╚══██╔══╝
+# ███████╗██║   ██║██║     ██║███████║██║         ██╔████╔██║█████╗  ██║  ██║██║███████║    ██╔████╔██║██║██╔██╗ ██║██║██╔██╗ ██║██║  ███╗       ██║   ██║   ██║██║   ██║██║     █████╔╝ ██║   ██║   
+# ╚════██║██║   ██║██║     ██║██╔══██║██║         ██║╚██╔╝██║██╔══╝  ██║  ██║██║██╔══██║    ██║╚██╔╝██║██║██║╚██╗██║██║██║╚██╗██║██║   ██║       ██║   ██║   ██║██║   ██║██║     ██╔═██╗ ██║   ██║   
+# ███████║╚██████╔╝╚██████╗██║██║  ██║███████╗    ██║ ╚═╝ ██║███████╗██████╔╝██║██║  ██║    ██║ ╚═╝ ██║██║██║ ╚████║██║██║ ╚████║╚██████╔╝       ██║   ╚██████╔╝╚██████╔╝███████╗██║  ██╗██║   ██║   
+# ╚══════╝ ╚═════╝  ╚═════╝╚═╝╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝╚══════╝╚═════╝ ╚═╝╚═╝  ╚═╝    ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝ ╚═════╝        ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝   ╚═╝   
+                                                                                                                                                                                                   
+# ██╗███████╗███╗   ███╗███╗   ███╗████████╗██╗                                                                                                                                                     
+# ██╔╝██╔════╝████╗ ████║████╗ ████║╚══██╔══╝╚██╗                                                                                                                                                    
+# ██║ ███████╗██╔████╔██║██╔████╔██║   ██║    ██║                                                                                                                                                    
+# ██║ ╚════██║██║╚██╔╝██║██║╚██╔╝██║   ██║    ██║                                                                                                                                                    
+# ╚██╗███████║██║ ╚═╝ ██║██║ ╚═╝ ██║   ██║   ██╔╝                                                                                                                                                    
+# ╚═╝╚══════╝╚═╝     ╚═╝╚═╝     ╚═╝   ╚═╝   ╚═╝     
+# 
+# Developed during Biomedical Hackathon 6 - http://blah6.linkedannotation.org/
+# Authors: Ramya Tekumalla, Javad Asl, Juan M. Banda
+# Contributors:
+
 try:
     import json
 except ImportError:
@@ -13,7 +31,6 @@ import time
 import tweepy
 from tweepy.parsers import RawParser
 import sys
-#import cld
 
 fhLog = codecs.open("LOG.txt",'a','UTF-8')
 def logPrint(s):
@@ -21,8 +38,8 @@ def logPrint(s):
 	print (s)
 
 #Update this line with the terms you want to search for
-terms =  ["funko"]
-
+input=sys.argv[1]
+terms = list(input.split(','))
 
 from auth import TwitterAuth
 
@@ -30,7 +47,7 @@ auth = tweepy.OAuthHandler(TwitterAuth.consumer_key, TwitterAuth.consumer_secret
 auth.set_access_token(TwitterAuth.access_token, TwitterAuth.access_token_secret)
 
 rawParser = RawParser()
-api = tweepy.API(auth_handler=auth, parser=rawParser)
+api = tweepy.API(auth_handler=auth, parser=tweepy.parsers.JSONParser())
 
 fhOverall=None
 allTweets = {}
@@ -41,42 +58,31 @@ for term in terms:
 	logPrint("Getting term %s (%s of %s)"%(term,termCnt,len(terms)))
 	minid=None #Lowest id we've seen so far, start at None
 	count=1
-	while True:
-		try:
-			fh=open(term+"_" + str(count) + ".json","r")
-			result=fh.read()
-			fh.close()
-			wait=0
-		except:	
-			if minid==None:
-				result=api.search(count=100,q=term,result_type="recent")
-			else:
-				result=api.search(count=100,q=term,max_id=minid,result_type="recent")
-			#The following will produce errors if the filesystem doesn't support characters used in the search term! (also above in try block)
-			fh=open(term+"_" + str(count) + ".json","w")
-			fh.write(result)
-			fh.close()
-			wait=5
-			
-		result=json.loads(result)
+	while True:	
+		if minid==None:
+			result=api.search(count=100,q=term,result_type="recent")
+		else:
+			result=api.search(count=100,q=term,max_id=minid,result_type="recent")
+		fh=open(term+"_" + str(count) + ".json","w")
+		limitS=len(result['statuses'])
+		for i in range(0,limitS):
+			fh.write(json.dumps(result['statuses'][i]) + "\n")
+		fh.close()
+		wait=5
 		if "statuses" in result and len(result["statuses"])>0:
-			logPrint("\nThere are %s results."%len(result["statuses"]))
+			logPrint("There are %s results."%len(result["statuses"]))
 			for status in result["statuses"]:
 				if minid==None or status["id"]<minid:
 					minid=status["id"]
 			count+=1
-			logPrint("Another page to get. Minimum id is %s"%minid)
+			#logPrint("Another page to get. A separate file will be written")
 		else:
 			minid=None
 			break
-		
 		#Deal with slight bug, if <=1 also quit
 		if "statuses" in result and len(result["statuses"])<=1:
 			minid=None
 			break
-
-		
 		time.sleep(wait)
-
 logPrint("\nDONE! Completed Successfully")
 fhLog.close()
