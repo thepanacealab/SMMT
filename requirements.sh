e70e1b35d695f3cddbc31f04f8bce02f6fbb933e
tramya28 <rtekumalla1@student.gsu.edu>
2020-05-04 19:23:45 -0400

    add requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+pip install spacy 
+python -m spacy download en
+python -m spacy download en_core_web_sm
+pip install twarc
+pip install tweepy
+pip install argparse
+pip install xtract
+pip install emoji
