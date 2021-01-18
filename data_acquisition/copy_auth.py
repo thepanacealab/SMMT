887dd0819d2d072737b903c5d64a3e2fe560d56d
tramya28 <git config --global user.email>
2021-01-18 14:19:27 -0500

    enable extended mode

--- data_acquisition/copy_auth.py
+++ data_acquisition/copy_auth.py
@@ -1,6 +1,6 @@
 class TwitterAuth:
         # Go to http://dev.twitter.com and create an app.
-        consumer_key = "gXCaD5RGWJUb4LDNUEcpJjpP2"
-        consumer_secret = "pnuAngrbkfRiWQkwO3nwLRMYs5TJKmXo98cxpAzcyyTVU21CKw"
-        access_token = "1173363240027508737-39JZstVKkKfP08ULvXJoT3q3uxQFDj"
-        access_token_secret = "ZM8koExLjPvSQoAcSkhnunPNRR4z6OJqjwhbOujsr9PoV"
+        consumer_key =  "YOURKEYGOESHERE"
+        consumer_secret =  "YOURKEYGOESHERE"
+        access_token =  "YOURKEYGOESHERE"
+        access_token_secret =  "YOURKEYGOESHERE"
